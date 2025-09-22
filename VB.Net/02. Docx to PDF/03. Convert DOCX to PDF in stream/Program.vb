8968178eda3911d10e14061c7bcc2e2798ceb33b
SautinSoft <support@sautinsoft.com>
2025-09-22 14:15:24 +0400

    New Code Samples

---
+++ VB.Net/02. Docx to PDF/03. Convert DOCX to PDF in stream/Program.vb
@@ -0,0 +1,42 @@
+﻿Option Infer On
+
+Imports System
+Imports System.IO
+Imports System.Collections
+
+Namespace Sample
+	Friend Class Test
+
+		Shared Sub Main(ByVal args() As String)
+			' Before starting, we recommend to get a free key:
+			' https://sautinsoft.com/start-for-free/
+
+			' Apply the key here:
+			' SautinSoft.PdfMetamorphosis.SetLicense("...");
+
+			Dim p As New SautinSoft.PdfMetamorphosis()
+
+			If p IsNot Nothing Then
+				Dim docxPath As String = "..\..\..\example.docx"
+				Dim pdfPath As String = Path.ChangeExtension(docxPath, ".pdf")
+				Dim docx As New MemoryStream(File.ReadAllBytes(docxPath))
+
+				' 3. Convert DOCX to PDF in stream                
+				Dim pdf As Stream = p.DocxToPdfConvertStream(docx)
+
+				If pdf IsNot Nothing Then
+					pdf.Position = 0
+					Dim ms = New MemoryStream()
+					pdf.CopyTo(ms)
+					ms.Position = 0
+					' 3. Save the PDF document to a file for a viewing purpose.
+					File.WriteAllBytes(pdfPath, ms.ToArray())
+					System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(pdfPath) With {.UseShellExecute = True})
+				Else
+					System.Console.WriteLine("Conversion failed!")
+					Console.ReadLine()
+				End If
+			End If
+		End Sub
+	End Class
+End Namespace
---
+++ VB.Net/04. HTML to PDF/05. Convert HTML to PDF in stream/Program.vb
@@ -0,0 +1,42 @@
+﻿Option Infer On
+
+Imports System
+Imports System.IO
+Imports System.Collections
+
+Namespace Sample
+	Friend Class Test
+
+		Shared Sub Main(ByVal args() As String)
+			' Before starting, we recommend to get a free key:
+			' https://sautinsoft.com/start-for-free/
+
+			' Apply the key here:
+			' SautinSoft.PdfMetamorphosis.SetLicense("...");
+
+			Dim p As New SautinSoft.PdfMetamorphosis()
+
+			If p IsNot Nothing Then
+				Dim htmlPath As String = "..\..\..\example.htm"
+				Dim pdfPath As String = Path.ChangeExtension(htmlPath, ".pdf")
+				Dim html As New MemoryStream(File.ReadAllBytes(htmlPath))
+
+				' 3. Convert HTML to PDF in stream                
+				Dim pdf As Stream = p.HtmlToPdfConvertStream(html)
+
+				If pdf IsNot Nothing Then
+					pdf.Position = 0
+					Dim ms = New MemoryStream()
+					pdf.CopyTo(ms)
+					ms.Position = 0
+					' 3. Save the PDF document to a file for a viewing purpose.
+					File.WriteAllBytes(pdfPath, ms.ToArray())
+					System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(pdfPath) With {.UseShellExecute = True})
+				Else
+					System.Console.WriteLine("Conversion failed!")
+					Console.ReadLine()
+				End If
+			End If
+		End Sub
+	End Class
+End Namespace
